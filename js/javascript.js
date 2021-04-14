21170f4477c484e6685c71480af19f1603a88ea5
kahfieidn <63580920+kahfieidn@users.noreply.github.com>
2021-04-14 13:59:31 +0700

    New

---
+++ js/javascript.js
@@ -0,0 +1,4 @@
+
+
+const mobile_menu = document.querySelector(".main-menu");
+mobile_menu.insertAdjacentHTML("afterend", "<div class='mobile-menu'><label class='show-menu' for='chk'><i class='fas fa-align-justify'></i></label><label class='close-menu' for='chk'><i class='fas fa-times'></i></label><label class='show-search' for='chk'><i class='fas fa-search'></i></label><label class='close-search' for='chk'><i class='fas fa-times'></i></label></div>");
